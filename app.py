b5e8c04c5c884c67124da80b6ba39902bdfd7e2d
hopefoundationtcpalya-wq <hopefoundationtcpalya@gmail.com>
2026-08-20 13:34:00 +0530

    added app.py file

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+import pickle
+import os
+
+app = Flask(_name_)
+
+# Load model and vectorizer
+model = pickle.load(open('model.pkl', 'rb'))
+vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    prediction = ""
+    if request.method == 'POST':
+        product_name = request.form['product']
+        product_vec = vectorizer.transform([product_name])
+        prediction = model.predict(product_vec)[0]
+    return render_template('index.html', prediction=prediction)
+
+if _name_ == '_main_':
+    app.run(debug=True, port=8080)
