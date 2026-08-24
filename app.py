d04b70d8ce035178294c8f5d6746faa476d1e281
hopefoundationtcpalya-wq <hopefoundationtcpalya@gmail.com>
2026-08-24 13:21:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import pickle
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 # Load model
 with open('model.pkl', 'rb') as f:
@@ -13,24 +13,24 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Accept both JSON and form data
     if request.is_json:
         data = request.get_json()
         text = data.get('text', '')
     else:
-        text = request.form.get('text', '') or request.form.get('description', '') or request.form.get('product', '')
+        text = request.form.get('text', '') or request.form.get('product_name', '') or request.form.get('description', '') or request.form.get('product', '')
         if not text and request.form:
             text = list(request.form.values())[0]
 
     if not text:
-        return render_template('index.html', prediction_text="Please enter product description", input_text="")
+        return render_template('index.html', prediction="Please enter product description", prediction_text="Please enter product description", input_text="")
 
     prediction = model.predict([text])[0]
 
     if request.is_json:
         return jsonify({'category': prediction})
     else:
-        return render_template('index.html', prediction_text=f"Predicted Category: {prediction}", input_text=text)
+        msg = f"Predicted Category: {prediction}"
+        return render_template('index.html', prediction=msg, prediction_text=msg, input_text=text)
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     app.run(debug=True)
